9c55770ad6e2024337f3626a3c41d1cb153f71cf
Jason Dorrell <j_dorrell0801@email.campbell.edu>
2017-10-04 11:57:09 -0400

    Saves json to local db AS-IS, no error OR format checking

---
+++ store_message.js
@@ -0,0 +1,16 @@
+﻿require('dotenv').config();
+
+const timestamp = require('unix-timestamp'),
+    JsonDb = require('node-json-db');
+
+module.exports.store = function (msg) {
+
+    var db = new JsonDb(process.env.INBOX, true, true),
+        msgID = "/" + timestamp.now();
+
+    db.push(msgID, msg);
+    console.log(msgID);
+
+
+    console.log("record added");
+};
